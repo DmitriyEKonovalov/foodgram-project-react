2601e3e6fc6f6716864f41d48aa6a71455de5254
Dmitriy E Konovalov <juns@mail.ru>
2022-08-18 23:09:12 +0300

    fix command create_admin

--- backend/api/management/commands/create_admin.py
+++ backend/api/management/commands/create_admin.py
@@ -9,11 +9,11 @@ class Command(BaseCommand):
     help = 'Create initial admin user'
 
     def handle(self, *args, **options):
-        User.objects.create_user(
-            username=getenv('DJANGO_SUPERUSER_USERNAME'),
-            email=getenv('DJANGO_SUPERUSER_EMAIL'),
-            password=getenv('DJANGO_SUPERUSER_PASSWORD'),
-            is_staff=True,
-            is_active=True,
-            is_superuser=True
-        )
+        User.objects.create_user({
+            'username': getenv('DJANGO_SUPERUSER_USERNAME'),
+            'email': getenv('DJANGO_SUPERUSER_EMAIL'),
+            'password': getenv('DJANGO_SUPERUSER_PASSWORD'),
+            'is_staff': True,
+            'is_active': True,
+            'is_superuser': True
+        })
