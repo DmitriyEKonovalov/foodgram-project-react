99a1ff7d8a2826abed2b6a1447c74214bee30e84
Dmitriy E Konovalov <juns@mail.ru>
2022-08-19 21:47:41 +0300

    move create users at command

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,12 +1,12 @@
 from django_filters.rest_framework import DjangoFilterBackend, FilterSet, CharFilter
-from django_filters.rest_framework import AllValuesMultipleFilter, ModelChoiceFilter, ChoiceFilter
+from django_filters.rest_framework import AllValuesMultipleFilter, ModelMultipleChoiceFilter, ModelChoiceFilter, ChoiceFilter
 
 from recipes.models import Recipe, Ingredient
 from users.models import User
 
 
 class RecipeFilter(FilterSet):
-    tags = AllValuesMultipleFilter(
+    tags = ModelMultipleChoiceFilter(
         field_name='tags__id')
     author = ModelChoiceFilter(
         queryset=User.objects.all())
