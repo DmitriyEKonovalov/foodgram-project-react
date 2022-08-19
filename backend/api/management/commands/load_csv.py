8e83fd0499eb7547c6f3782e8d1d6fc9ee56a0fc
Dmitriy E Konovalov <juns@mail.ru>
2022-08-19 20:30:45 +0300

    add users to load_csv

--- backend/api/management/commands/load_csv.py
+++ backend/api/management/commands/load_csv.py
@@ -2,11 +2,14 @@ import csv
 from pathlib import Path
 from django.conf import settings
 from django.core.management import BaseCommand
+
 from recipes.models import Ingredient, Tag, Recipe
+from users.models import User
 
 TABLES = [
     (Ingredient, 'ingredients.csv'),
     (Tag, 'tags.csv'),
+    (User, 'users.csv'),
     (Recipe, 'recipes.csv'),
 
 ]
