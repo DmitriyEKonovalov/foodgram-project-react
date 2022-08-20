33d7c7a825c3dc5e3e667465c9fae971f5d1aef5
Dmitriy E Konovalov <juns@mail.ru>
2022-08-20 18:22:13 +0300

    fix load_csv

--- backend/api/management/commands/load_csv.py
+++ backend/api/management/commands/load_csv.py
@@ -3,14 +3,19 @@ from pathlib import Path
 from django.conf import settings
 from django.core.management import BaseCommand
 
-from recipes.models import Ingredient, Tag, Recipe, Favorite, RecipeIngredient, ShoppingCart
+from recipes.models import (Favorite, Ingredient, Recipe,
+                            RecipeIngredient, ShoppingCart, Subscribe, Tag)
+
 from users.models import User
 
 TABLES = [
     (Ingredient, 'ingredients.csv'),
     (Tag, 'tags.csv'),
     (Recipe, 'recipes.csv'),
-
+    (RecipeIngredient, 'recipe_ingredients.csv'),
+    (Favorite, 'favorites.csv'),
+    (ShoppingCart, 'shoppingcarts.csv'),
+    (Subscribe, 'subscribes.csv'),
 ]
 
 
@@ -18,12 +23,21 @@ class Command(BaseCommand):
     help = 'Load data from csv files'
 
     def handle(self, *args, **kwargs):
+
         for model, filename in TABLES:
             file_path = Path(settings.BASE_DIR).parent.joinpath('data').joinpath(filename)
             self.stdout.write(self.style.MIGRATE_LABEL(f'start loading {filename}'))
             with open(file_path, 'r', encoding='utf-8') as csv_file:
                 reader = csv.DictReader(csv_file)
                 model.objects.bulk_create(model(**data) for data in reader)
+
+        self.stdout.write(self.style.MIGRATE_LABEL(f'start loading recipe_tags.csv'))
+        file_path = Path(settings.BASE_DIR).parent.joinpath('data').joinpath('recipe_tags.csv')
+        with open(file_path, 'r', encoding='utf-8') as csv_file:
+            reader = csv.DictReader(csv_file)
+            for row in reader:
+                recipe = Recipe.objects.get(id=row['recipe_id'])
+                recipe.tags.set(row['tag_id'], clear=False)
 
         self.stdout.write(self.style.SUCCESS('Data loaded successfully'))
 
