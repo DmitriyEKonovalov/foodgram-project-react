6b4115e2842a9b349bf1429afecedd7e564fe32d
Dmitriy E Konovalov <juns@mail.ru>
2022-08-21 18:53:08 +0300

    fix static path nginx

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -138,8 +138,8 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_URL = '/backend_static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'backend_static')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/backend_media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'backend_media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
